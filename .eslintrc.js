111733fc687e32ee7c27ca33ec807135ae26c899
Martin Fouček <martin.foucek@emplifi.io>
2024-05-31 19:35:52 +0200

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+	env: {
+		node: true,
+		es2023: true,
+	},
+	extends: ['eslint:recommended', 'prettier'],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+	semi: false,
+	singleQuote: true,
+	tabWidth: 4,
+	useTabs: true,
+}
+
+module.exports = config
