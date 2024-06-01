67b13e9dca811592b2188280b1c63fc61344129d
Martin Fouček <martin.foucek@emplifi.io>
2024-06-01 20:23:14 +0200

    Fix activity icons in production build

--- client/components/summary.jsx
+++ client/components/summary.jsx
@@ -1,6 +1,22 @@
 import Km from './km'
 import React from 'react'
+import iconBicycle from '../assets/bicycle.png'
+import iconInline from '../assets/inline.png'
+import iconRunning from '../assets/running.png'
+import iconSwimming from '../assets/swimming.png'
+import iconWalking from '../assets/walking.png'
 import './summary.css'
+
+/**
+ * @param {keyof import('./app').Sports} sport
+ */
+const getIcon = (sport) => {
+	if (sport === 'bicycle') return iconBicycle
+	if (sport === 'inline') return iconInline
+	if (sport === 'running') return iconRunning
+	if (sport === 'swimming') return iconSwimming
+	if (sport === 'walking') return iconWalking
+}
 
 /**
  * @param {{ activities: import('./app').Activity[], recentDays: number}} props
@@ -44,13 +60,7 @@ function Summary({ activities, recentDays }) {
 													{count}x
 												</span>
 												&nbsp;
-												<img
-													src={
-														'assets/' +
-														sport +
-														'.png'
-													}
-												/>
+												<img src={getIcon(sport)} />
 											</span>
 										)
 									})}
