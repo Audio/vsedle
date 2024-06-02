e625b6a1b696c18e289704a49119c515681bcb62
Martin Fouček <martin.foucek@emplifi.io>
2024-06-02 17:12:51 +0200

    Round kms to 1 place

--- client/components/km.jsx
+++ client/components/km.jsx
@@ -4,7 +4,7 @@ import React from 'react'
  * @param {{distance: number}} props
  */
 function Km({ distance }) {
-	return <>{distance.toFixed(2)} km</>
+	return <>{distance.toFixed(1)} km</>
 }
 
 export default Km
